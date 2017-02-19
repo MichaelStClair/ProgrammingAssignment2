827fd2f91e59adc60db233082531218367997b9b
Mike St. Clair <mikestclair@gmail.com>
2017-02-19 18:19:11 -0500

    R Programming Week 3 (Coursera) Assignment
    Function and object to invert a square matrix and cache results

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,15 @@
 ## Homework assignment for week 3 of R Programming (Coursera)
 ##   (the name of the assignment is ProgrammingAssignment2)
+## Coded by Michael St. Clair 2017-02-19
 
 ## Create a matrix object with four methods
 ## it can set itself
 ## it can get itself
-## it can solve itself aka set its own solution
+## it can solve (invert) itself aka set its own solution
 ## it can get the solution of itself
 ##
 ## this uses the standard solve() function so the matrix must be square!
+## and the matrix must be invertible (permitted assumption of assignment)
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -23,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Create a function to get solution of above matrix object
+## Create a function to get solution (inversion) of above matrix object
 ## If already solved, return a cached version of the solution
 ## by ONLY using the setsolve method when the getsolve method returns null
 cacheSolve <- function(x, ...) {
